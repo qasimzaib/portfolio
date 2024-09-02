eb6dcb08fa6fd0ffb137dae87151203b0edef04b
qasimzaib <zaib_qasim@hotmail.com>
2024-09-02 18:58:50 +0500

    Added starry background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { siteMetaData } from '@/config/site-meta-data';
 import './globals.css';
 import { Navbar } from '@/components/molecules/navbar';
 import { Footer } from '@/components/molecules/footer';
+import { BackgroundCanvas } from '@/components/molecules/background-canvas';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
 					inter.className
 				)}
 			>
+				<BackgroundCanvas />
 				<Navbar />
 				{children}
 				<Footer />
