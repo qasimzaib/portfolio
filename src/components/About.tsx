55e8d359343c7b40262bfe12441c6f0f809248b0
qasimzaib <zaib_qasim@hotmail.com>
2025-04-25 22:40:11 +0500

    Husky working ok

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,4 @@
 export default function About() {
-	let abc = 12345;
-
 	return (
 		<section id="about" className="py-20 container mx-auto px-6">
 			<h2 className="text-3xl font-bold mb-6">About</h2>
